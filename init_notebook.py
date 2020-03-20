d28df24319e80fe2a06ecbfc67a7c17180791492
Sylvain GUTIERREZ <sylvain.gut@gmail.com>
2020-03-20 11:04:54 +0100

    ajout du gitignore et d.un premier notebook d.analyse pour verifier la lecture des donnees

---
+++ init_notebook.py
@@ -0,0 +1,10 @@
+#import this file to initialize your notebook with any library you may need
+
+import numpy as np
+import pandas as pd
+import sklearn as sk
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+def init():
+	pass
